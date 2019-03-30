e128b7cfe3bdcb4e5c868d5e4f492f8ed4561f0c
Roman Yephimov <ry@piogroup.net>
2019-03-30 21:16:28 +0200

    init commit

--- src/control-panel/ControlPanel.js
+++ src/control-panel/ControlPanel.js
@@ -2,13 +2,51 @@ import React, { Component } from 'react';
 import './ControlPanel.css';
 
 class ControlPanel extends Component {
+
+    activeWord = null;
+
+    isSelected = () => !!window.getSelection().toString();
+
+    setStyle = (property) => {
+        if(this.activeWord && this.isSelected()) {
+            this.activeWord.classList.contains(property) ?
+                this.activeWord.classList.remove(property)
+                :
+                this.activeWord.classList.add(property);
+        }
+    };
+
+    componentDidMount() {
+        document.addEventListener('dblclick', event => {
+            this.activeWord = event.target.id ? event.target : null;
+        });
+    }
+
     render() {
         return (
             <div id="control-panel">
                 <div id="format-actions">
-                    <button className="format-action" type="button"><b>B</b></button>
-                    <button className="format-action" type="button"><i>I</i></button>
-                    <button className="format-action" type="button"><u>U</u></button>
+                    <button
+                        onClick={() => this.setStyle('fontWeight')}
+                        className="format-action"
+                        type="button"
+                    >
+                        <b>B</b>
+                    </button>
+                    <button
+                        onClick={() => this.setStyle('fontStyle')}
+                        className="format-action"
+                        type="button"
+                    >
+                        <i>I</i>
+                    </button>
+                    <button
+                        onClick={() => this.setStyle('textDecoration')}
+                        className="format-action"
+                        type="button"
+                    >
+                        <u>U</u>
+                    </button>
                 </div>
             </div>
         );
--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import './FileZone.css';
 
 class FileZone extends Component {
+
+    state = {
+        textareaValue: '',
+    };
+
+    getHtml = () => (
+        this.state.textareaValue ? this.state.textareaValue.split(' ').map((item, key) => (
+                <span
+                    key={key}
+                    id={key}
+                >
+                    {item}&nbsp;
+                </span>
+            )
+        ) : ""
+    );
+
+    textareaChange = e => {
+        this.setState({ textareaValue: e.target.value })
+    };
+
     render() {
+        const { textareaValue } = this.state;
         return (
             <div id="file-zone">
                 <div id="file">
-
+                    <textarea spellCheck={false} value={textareaValue} onChange={this.textareaChange} className="textarea" />
+                    <div className="text-view">
+                        {this.getHtml()}
+                    </div>
                 </div>
             </div>
         );
